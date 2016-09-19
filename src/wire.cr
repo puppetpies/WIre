bc932b89c3bd747ae67e70ef2b697d307a8b1771
Bri in The Sky <brianh6854@googlemail.com>
2016-09-19 12:50:11 +0100

    Merged options / tweaks from dbbackends

--- src/wire.cr
+++ src/wire.cr
@@ -29,8 +29,10 @@ module Wire
   dataonly = false
   bodymode = false
   filemode = false
-  dumpfile = ""
-  separatorlen = 100
+  hexdump = false
+  whitespace = false
+  dumpfile = "" # Read in a LibPcap created capture file instead of interfaces also works with the database
+  separatorlen = 100_u8
   banner = "WIre version #{VERSION}\n\nUsage: WIre [options] | -h for Help\n"
   
   opts = OptionParser.new do |parser|
@@ -38,10 +40,12 @@ module Wire
     parser.on("-i lo", "Listen on interface") { |i| device = i }
     parser.on("-f 'tcp port 80'", "Pcap filter string. See pcap-filter(7)"  ) { |f| filter = f }
     parser.on("-s 1500", "Snapshot length"  ) { |s| snaplen = s.to_i }
-    parser.on("-o dumpfile", "Open pcap dump file") {|d| dumpfile = d; filemode = true }
+    parser.on("-r file", "Read packets from file") {|d| dumpfile = d; filemode = true }
     parser.on("-d", "Filter packets where tcp data exists") { dataonly = true }
     parser.on("-b", "Body printing mode"    ) { bodymode = true }
     parser.on("-v", "Show verbose output"   ) { verbose  = true }
+    parser.on("-w", "Ignore all packets that contain only white spaces") { whitespace = true }
+    parser.on("-x", "Hexdump") { hexdump  = true }
     parser.on("-h", "--help", "Show help"   ) { puts parser; exit 0 }
   end
 
@@ -55,6 +59,7 @@ module Wire
     display_option("Snaplen", snaplen)
     display_option("Verbose", verbose) 
     display_option("Dataonly", dataonly)
+    display_option("Hexdump", hexdump)
     
     unless filemode == true
       cap = Pcap::Capture.open_live(device, snaplen: snaplen, timeout_ms: timeout)
@@ -67,16 +72,19 @@ module Wire
     cap.loop do |pkt|
       next if dataonly && !pkt.tcp_data
       if bodymode
+        next if whitespace && pkt.tcp_data.to_s =~ /\A\s*\Z/
         puts "%s: %s" % [pkt.packet_header, pkt.tcp_data.to_s.inspect]
       else
-        #puts pkt.to_s
-        puts "-" * separatorlen
-        puts display(pkt.src, pkt.dst)
-        puts "-" * separatorlen
-        puts pkt.inspect if verbose
-        #puts "-" * separatorlen     if verbose
-        #puts pkt.inspect  if verbose
-        #puts pkt.hexdump
+        case hexdump
+        when false
+          puts "-" * separatorlen
+          puts display(pkt.src, pkt.dst)
+          puts "-" * separatorlen
+          puts pkt.inspect if verbose
+        else
+          puts "#{pkt.packet_header} #{pkt.src} #{pkt.dst}".colorize(:yellow)
+          puts pkt.hexdump
+        end
       end
     end
   rescue err
