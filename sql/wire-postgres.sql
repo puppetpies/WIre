da4340e17449cbfa726ef17d26393d627cdbf9af
Bri in The Sky <brianh6854@googlemail.com>
2016-09-25 13:52:12 +0100

    Fixed character type should be variable character length

--- sql/wire-postgres.sql
+++ sql/wire-postgres.sql
@@ -1,6 +1,7 @@
 --
 --  WIre schema for Postgres tested on 9.5.4
 --
+SET search_path TO wire;
 
 CREATE USER wire WITH PASSWORD 'changeme';
 CREATE SCHEMA wire AUTHORIZATION wire;
@@ -9,16 +10,16 @@ DROP TABLE IF EXISTS wire.ippacket;
 CREATE TABLE wire.ippacket (
   id integer UNIQUE NOT NULL,
   recv_date TIMESTAMP WITHOUT TIME ZONE,
-  ip_df character(5) NOT NULL,
-  ip_dst character(15) DEFAULT NULL,
+  ip_df VARCHAR(5) NOT NULL,
+  ip_dst VARCHAR(15) DEFAULT NULL,
   ip_hlen integer NOT NULL,
   ip_id integer NOT NULL,
   ip_len integer NOT NULL,
-  ip_mf character(5) DEFAULT NULL,
+  ip_mf VARCHAR(5) DEFAULT NULL,
   ip_off integer NOT NULL,
   ip_proto integer NOT NULL,
-  ip_src character(15) DEFAULT NULL,
-  ip_sum character(10) DEFAULT NULL,
+  ip_src VARCHAR(15) DEFAULT NULL,
+  ip_sum VARCHAR(10) DEFAULT NULL,
   ip_tos integer NOT NULL,
   ip_ttl integer NOT NULL,
   ip_ver integer NOT NULL
@@ -42,10 +43,10 @@ CREATE TABLE wire.tcppacket (
   tcp_off integer DEFAULT NULL,
   tcp_hlen integer DEFAULT NULL,
   tcp_seq bigint DEFAULT NULL,
-  tcp_sum character(10) DEFAULT NULL,
+  tcp_sum VARCHAR(10) DEFAULT NULL,
   tcp_sport integer DEFAULT NULL,
-  tcp_urp character(10) DEFAULT NULL,
-  tcp_win character(10) DEFAULT NULL
+  tcp_urp VARCHAR(10) DEFAULT NULL,
+  tcp_win VARCHAR(10) DEFAULT NULL
 );
 GRANT SELECT, INSERT, DELETE ON wire.tcppacket TO wire;
 
@@ -56,7 +57,7 @@ CREATE TABLE wire.udppacket (
   udp_data bytea,
   udp_dport integer DEFAULT NULL,
   udp_len integer DEFAULT NULL,
-  udp_sum character(10) DEFAULT NULL,
+  udp_sum VARCHAR(10) DEFAULT NULL,
   udp_sport integer DEFAULT NULL
 );
 GRANT SELECT, INSERT, DELETE ON wire.udppacket TO wire;
