4dc721f3bedff59b460378ae2c0f9a8de42106fb
Bri in The Sky <brianh6854@googlemail.com>
2016-09-25 13:17:17 +0100

    Added MonetDB HTTP Body schema

---
+++ sql/wire-monetdb-httpbody.sql
@@ -0,0 +1,36 @@
+SET SCHEMA "threatmonitor";
+
+DROP TABLE "threatmonitor".http_traffic_json;
+CREATE TABLE "threatmonitor".http_traffic_json (
+id INT GENERATED ALWAYS AS 
+        IDENTITY (
+           START WITH 0 INCREMENT BY 1
+           NO MINVALUE NO MAXVALUE
+           CACHE 2 CYCLE
+) PRIMARY KEY,
+  guid CHAR(36) NOT NULL,
+  recv_date DATE,
+  recv_time TIME,
+  json_data JSON
+);
+
+CREATE INDEX index_traffic_json_id ON "threatmonitor".http_traffic_json(id);
+CREATE INDEX index_traffic_json_guid ON "threatmonitor".http_traffic_json(guid);
+
+DROP TABLE "threatmonitor".http_traffic_ua;
+CREATE TABLE "threatmonitor".http_traffic_ua (
+id INT GENERATED ALWAYS AS 
+        IDENTITY (
+           START WITH 0 INCREMENT BY 1
+           NO MINVALUE NO MAXVALUE
+           CACHE 2 CYCLE
+) PRIMARY KEY,
+  family VARCHAR(30),
+  major CHAR(10) default 'NaN',
+  minor CHAR(10) default 'NaN',
+  os CHAR(30) NOT NULL,
+  guid CHAR(36) NOT NULL
+);
+
+CREATE INDEX index_traffic_ua_id ON "threatmonitor".http_traffic_ua(id);
+CREATE INDEX index_traffic_ua_guid ON "threatmonitor".http_traffic_ua(guid);
