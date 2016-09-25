2fa3c7ec3e81d4cf7dd5cadd20578607de2eea41
Bri in The Sky <brianh6854@googlemail.com>
2016-09-25 13:24:17 +0100

    Added MySQL HTTP Body schema

---
+++ sql/wire-mysql-httpbody.sql
@@ -0,0 +1,22 @@
+--
+--  WIre HTTP Body schema for MySQL
+--
+
+DROP TABLE http_traffic_json;
+CREATE TABLE http_traffic_json (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  guid CHAR(36) NOT NULL,
+  recv_date DATE,
+  recv_time TIME,
+  json_data TEXT
+);
+
+DROP TABLE http_traffic_ua;
+CREATE TABLE http_traffic_ua (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  family VARCHAR(30),
+  major CHAR(10) DEFAULT 'NaN',
+  minor CHAR(10) DEFAULT 'NaN',
+  os CHAR(30) NOT NULL,
+  guid CHAR(36) NOT NULL
+);
