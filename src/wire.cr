2b225ef7e4e952179050621e9c42348d9fd794a1
Bri in The Sky <brianh6854@googlemail.com>
2016-10-24 16:53:49 +0100

    INSERTs now work for all databases tested

--- src/wire.cr
+++ src/wire.cr
@@ -23,9 +23,6 @@ require "option_parser"
 require "secure_random"
 
 module Wire
-  # Connection error / banners
-  CONNERR = ">> Connection error"
-  CONNBANNER = ">> Connected to database on"
   
   # Variables to use with Options parser
   filter   = "tcp port 80" # PCAP-FILTER(7) man page for details or TSHARK(1) via wireshark
@@ -38,6 +35,7 @@ module Wire
   filemode = false
   hexdump = false
   whitespace = false
+  quiet = false
   dumpfile = "" # Read in a LibPcap created capture file instead of interfaces also works with the database
   separatorlen = 100_u8
   banner = "WIre version #{VERSION}\n\nUsage: WIre [options] | -h for Help\n"
@@ -53,73 +51,42 @@ module Wire
     parser.on("-v", "Show verbose output") { verbose  = true }
     parser.on("-w", "Ignore all packets that contain only white spaces") { whitespace = true }
     parser.on("-x", "Hexdump") { hexdump  = true }
+    parser.on("-q", "Quiet") { quiet = true }
     parser.on("-h", "--help", "Show help") { puts parser; exit 0 }
   end
   
-  def self.ip(schema : String, guid : String, tbl : String, ip_dst : String, ip_hlen : Number, ip_id : Number, ip_len : Number, ip_proto : Number, ip_src : String, ip_sum : Number, ip_tos : Number, ip_ttl : Number, ip_ver : Number)
-    io = "INSERT INTO #{schema}.#{tbl} "
-    io += "(guid, recv_date, recv_time, ip_dst, ip_hlen, ip_id, ip_len, ip_proto, ip_src, ip_sum, ip_tos, ip_ttl, ip_ver) "
-    io += "VALUES ("
-    io += "'#{guid}', "
-    io += "NOW(), "
-    io += "NOW(), "
-    io += "'#{ip_dst}',"
-    io += "'#{ip_hlen}',"
-    io += "'#{ip_id}',"
-    io += "'#{ip_len}',"
-    io += "'#{ip_proto}',"
-    io += "'#{ip_src}',"
-    io += "'#{ip_sum}',"
-    io += "'#{ip_tos}',"
-    io += "'#{ip_ttl}',"
-    io += "'#{ip_ver}');"
-  end
-  
-  def self.tcp(schema : String, guid : String, tbl : String, tcp_data_len : Int32, tcp_dst : UInt16, tcp_ack : Bool, tcp_fin : Bool, tcp_syn : Bool, tcp_rst : Bool, tcp_psh : Bool, tcp_urg : Bool, tcp_off : Int32, tcp_hlen : Int32, tcp_seq : UInt32, tcp_sum : UInt16, tcp_src : UInt16, tcp_win : UInt16)
-    io = "INSERT INTO #{schema}.#{tbl} "
-    io += "(guid, recv_date, recv_time, tcp_data_len, tcp_dport, tcp_ack, tcp_fin, tcp_syn, tcp_rst, tcp_psh, tcp_urg, tcp_off, tcp_hlen, tcp_seq, tcp_sum, tcp_sport, tcp_win) "
-    io += "VALUES ("
-    io += "'#{guid}', "
-    io += "NOW(), "
-    io += "NOW(), "
-    io += "#{tcp_data_len},"
-    io += "#{tcp_dst},"
-    io += "'#{tcp_ack}',"
-    io += "'#{tcp_fin}',"
-    io += "'#{tcp_syn}',"
-    io += "'#{tcp_rst}',"
-    io += "'#{tcp_psh}',"
-    io += "'#{tcp_urg}',"
-    io += "#{tcp_off}, #{tcp_hlen}, #{tcp_seq}, #{tcp_sum}, #{tcp_dst}, #{tcp_win});"
-  end
-
   begin
+    self.createpid # Create process id
     config_json_data = Wire.load_config # Read the json config of your DB details
     j = Jq.new(config_json_data)
-    case j[".driver"].as_s
-    when "monetdb"
-      conn = MonetDB::Client.new
-      conn.host = j[".host"].as_s
-      conn.port = j[".port"].as_i
-      conn.username = j[".username"].as_s
-      conn.password = j[".password"].as_s
-      conn.db = j[".db"].as_s
-      conn.connect
-      conn.setAutocommit(true)
-      p conn
-    else
-      conn = MonetDB::Client.new
-    end
+    commit_after = j[".commit_interval"].as_i # How many records before commit
+    driver = j[".driver"].as_s
+    
+    # MonetDB
+    #conn = MonetDB::Client.new
+    #conn.connect(j[".host"].as_s, j[".username"].as_s, j[".password"].as_s, j[".db"].as_s, j[".port"].as_i)
+    #conn.setAutocommit(false)
+    #p conn
+    
+    # MySQL
+    #conn = MySQL.connect(j[".host"].as_s, j[".username"].as_s, j[".password"].as_s, j[".db"].as_s, j[".port"].as_i)
+    #p conn
+    
+    # Postgres
+    conninfo = PQ::ConnInfo.new(j[".host"].as_s, j[".db"].as_s, j[".username"].as_s, j[".password"].as_s, j[".port"].as_i)
+    conn = PG.connect(conninfo)
+    p conn
+    
     opts.parse!
     puts banner.colorize(:cyan)
     puts "Starting up!".colorize(:red)
     puts "============\n".colorize(:red)
-    display_option("Filter", filter)
-    display_option("Device", device)
-    display_option("Snaplen", snaplen)
-    display_option("Verbose", verbose) 
-    display_option("Dataonly", dataonly)
-    display_option("Hexdump", hexdump)
+    display("Filter", filter)
+    display("Device", device)
+    display("Snaplen", snaplen)
+    display("Verbose", verbose) 
+    display("Dataonly", dataonly)
+    display("Hexdump", hexdump)
     
     unless filemode == true
       cap = Pcap::Capture.open_live(device, snaplen: snaplen, timeout_ms: timeout)
@@ -128,9 +95,10 @@ module Wire
       puts "#{dumpfile}"
       cap = Pcap::Capture.open_offline(dumpfile)
     end
-    at_exit { cap.close }
+
     cap.setfilter(filter)
     pktcount = 0
+    glbpktcount = 0
     cap.loop do |pkt|
       next if dataonly && !pkt.tcp_data
       if bodymode
@@ -138,27 +106,49 @@ module Wire
         puts "%s: %s" % [pkt.packet_header, pkt.tcp_data.to_s.inspect]
       else
         # Data capture
-        sameuuid = SecureRandom.uuid
+        sameuuid = SecureRandom.uuid # Needs to be the same for JOIN query
         sql_ip = ip("#{j[".schema"].as_s}", sameuuid, "#{j[".tbl_ippacket"].as_s}", pkt.ip_dst, pkt.ip_hl, pkt.ip_id, pkt.ip_len, pkt.ip_proto, pkt.ip_src, pkt.ip_sum, pkt.ip_tos, pkt.ip_ttl, pkt.ip_v)
         sql_tcp = tcp("#{j[".schema"].as_s}", sameuuid, "#{j[".tbl_tcppacket"].as_s}", pkt.tcp_data_len, pkt.tcp_dst, pkt.tcp_ack?, pkt.tcp_fin?, pkt.tcp_syn?, pkt.tcp_rst?, pkt.tcp_push?, pkt.tcp_urg?, pkt.tcp_doff, pkt.tcp_hlen, pkt.tcp_seq, pkt.tcp_sum, pkt.tcp_src, pkt.tcp_win)
-        puts "#{pktcount}: IP: #{sql_ip}"
-        puts "#{pktcount}: TCP: #{sql_tcp}"
-        pktcount += 1
-        conn.query(sql_ip)
-        conn.query(sql_tcp)
-        puts "-" * separatorlen
+        puts "#{glbpktcount}: IP: #{sql_ip}" if verbose
+        puts "#{glbpktcount}: TCP: #{sql_tcp}" if verbose
+        conn.exec(sql_ip)
+        conn.exec(sql_tcp)
         case hexdump
         when false
-          puts "-" * separatorlen
-          puts display(pkt.src, pkt.dst)
-          puts "-" * separatorlen
+          case pkt.tcp_data.to_s.size
+          when 0
+            data = false
+          else
+            data = true
+          end
+          puts display(pkt.src, pkt.dst, pkt.tcp_ack?, pkt.tcp_fin?, pkt.tcp_syn?, pkt.tcp_rst?, pkt.tcp_push?, pkt.tcp_urg?, pkt.tcp_doff, pkt.tcp_hlen, pkt.tcp_seq, pkt.tcp_sum, pkt.tcp_win, data) unless quiet
+          puts "-" * separatorlen unless quiet
           puts pkt.inspect if verbose
         else
           puts "#{pkt.packet_header} #{pkt.src} #{pkt.dst}".colorize(:yellow)
           puts pkt.hexdump
         end
       end
+      if commit_after == pktcount
+        begin
+          conn.exec("COMMIT;")
+          puts "Commit #{pktcount} !".colorize(:yellow) if verbose
+          pktcount = 0
+        rescue
+          abort "Commit failure".colorize(:red)
+        end
+      end
+      glbpktcount += 1
+      pktcount += 1
     end
+    at_exit { 
+      conn.exec("COMMIT;")
+      cap.close 
+      print "Total Packets: "
+      print "#{glbpktcount}".colorize(:white)
+      puts ""
+      self.removepid # Delete process id file
+    }
   rescue err
     STDERR.puts "#{$0}: #{err}" # The Catch all
   end
