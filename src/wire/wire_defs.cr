cc38ebd076020ea0aaf268d9c0fc2c92d81d36fa
Bri in The Sky <brianh6854@googlemail.com>
2016-10-24 16:52:55 +0100

    Moved / Added INSERTS for DB Backends & Added PID Functions

--- src/wire/wire_defs.cr
+++ src/wire/wire_defs.cr
@@ -1,7 +1,49 @@
 module Wire
-
-  @@configfile = "config.json"
+  # Connection error / banners
+  CONNERR = ">> Connection error"
+  CONNBANNER = ">> Connected to database on"
   
+  @@configfile = "config-postgres.json"
+
+  # Database functions
+  def self.ip(schema : String, guid : String, tbl : String, ip_dst : String, ip_hlen : Number, ip_id : Number, ip_len : Number, ip_proto : Number, ip_src : String, ip_sum : Number, ip_tos : Number, ip_ttl : Number, ip_ver : Number)
+    io = "INSERT INTO #{schema}.#{tbl} "
+    io += "(guid, recv_date, recv_time, ip_dst, ip_hlen, ip_id, ip_len, ip_proto, ip_src, ip_sum, ip_tos, ip_ttl, ip_ver) "
+    io += "VALUES ("
+    io += "'#{guid}', "
+    io += "NOW(), "
+    io += "NOW(), "
+    io += "'#{ip_dst}',"
+    io += "#{ip_hlen},"
+    io += "#{ip_id},"
+    io += "#{ip_len},"
+    io += "#{ip_proto},"
+    io += "'#{ip_src}',"
+    io += "#{ip_sum},"
+    io += "#{ip_tos},"
+    io += "#{ip_ttl},"
+    io += "#{ip_ver});"
+  end
+  
+  def self.tcp(schema : String, guid : String, tbl : String, tcp_data_len : Int32, tcp_dst : UInt16, tcp_ack : Bool, tcp_fin : Bool, tcp_syn : Bool, tcp_rst : Bool, tcp_psh : Bool, tcp_urg : Bool, tcp_off : Int32, tcp_hlen : Int32, tcp_seq : UInt32, tcp_sum : UInt16, tcp_src : UInt16, tcp_win : UInt16)
+    io = "INSERT INTO #{schema}.#{tbl} "
+    io += "(guid, recv_date, recv_time, tcp_data_len, tcp_dport, tcp_ack, tcp_fin, tcp_syn, tcp_rst, tcp_psh, tcp_urg, tcp_off, tcp_hlen, tcp_seq, tcp_sum, tcp_sport, tcp_win) "
+    io += "VALUES ("
+    io += "'#{guid}', "
+    io += "NOW(), "
+    io += "NOW(), "
+    io += "#{tcp_data_len},"
+    io += "#{tcp_dst},"
+    io += "'#{tcp_ack}',"
+    io += "'#{tcp_fin}',"
+    io += "'#{tcp_syn}',"
+    io += "'#{tcp_rst}',"
+    io += "'#{tcp_psh}',"
+    io += "'#{tcp_urg}',"
+    io += "#{tcp_off}, #{tcp_hlen}, #{tcp_seq}, #{tcp_sum}, #{tcp_dst}, #{tcp_win});"
+  end
+
+  # Tools / Config
   def self.check_permission?
     perm = %x(id -u)[0..0].to_i
     unless perm == 0
@@ -40,15 +82,39 @@ module Wire
     return json_data
   end
 
-  def self.display(src : String, dst : String)
+  # Display modes
+  def self.display(src : String, dst : String, tcp_ack : Bool, tcp_fin : Bool, tcp_syn : Bool, tcp_rst : Bool, tcp_push : Bool, tcp_urg : Bool, tcp_doff : Int32, tcp_hlen : Int32, tcp_seq : UInt32, tcp_sum : UInt16, tcp_win : UInt16, data : Bool)
     io = "#{Time.now.to_s} "
-    io += "Src IP Addr: #{src.colorize(:yellow)} "
-    io += "Dst IP Addr: #{dst.colorize(:yellow)} "
+    io += "IP Src: #{src.colorize(:white)} "
+    io += "IP Dst: #{dst.colorize(:white)} "
+    io += "ACK: #{tcp_ack.colorize(:white)} "
+    io += "SYN: #{tcp_syn.colorize(:white)} "
+    io += "FIN: #{tcp_fin.colorize(:white)} "
+    io += "RST: #{tcp_rst.colorize(:white)} "
+    io += "PUSH: #{tcp_push.colorize(:white)} "
+    io += "URG: #{tcp_ack.colorize(:white)} "
+    io += "Offset: #{tcp_doff.colorize(:white)} "
+    io += "Header Length: #{tcp_hlen.colorize(:white)} "
+    io += "Sequence Num: #{tcp_seq.colorize(:white)} "
+    io += "Checksum: #{tcp_sum.colorize(:white)} "
+    io += "Window Size: #{tcp_win.colorize(:white)} "
+    io += "Has Data?: #{data} "
   end
 
-  def self.display_option(name : String, var : String | Bool | Int32)
+  def self.display(name : String, var : String | Bool | Int32)
     print "#{name.camelcase}: ".colorize(:blue)
     print "#{var}\n".colorize(:white)
+  end
+  
+  # PID Functions
+  def self.createpid
+    File.open("/var/run/wire.pid", "w") {|n|
+      n.print("#{Process.pid}")
+    }
+  end
+  
+  def self.removepid
+    File.delete("/var/run/wire.pid")
   end
   
 end
